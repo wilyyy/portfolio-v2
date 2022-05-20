54ca719cff8b6b6ac2a6b32f818e46b1e46c5b41
wilyyy <walvarez1@my.bcit.ca>
2022-05-20 15:07:30 -0700

    close button

--- src/components/ContentModals/ContentModal.js
+++ src/components/ContentModals/ContentModal.js
@@ -1,12 +1,13 @@
 import styled from "styled-components";
 import { motion, AnimatePresence } from "framer-motion";
 
-import { useTheme } from "@/src/utils/AppProvider";
+import { useTheme, useStatus } from "@/src/utils/AppProvider";
 import { globalTheme } from "@/src/utils/ThemeConfig";
 import { ModalInit } from "@/src/utils/Animations";
 
 export default function ContentModal({ children }) {
    const { theme } = useTheme();
+   const { status, setStatus } = useStatus();
 
    return (
       <AnimatePresence>
@@ -22,14 +23,22 @@ export default function ContentModal({ children }) {
             border={globalTheme[theme].border}
          >
             <Bottom>{children}</Bottom>
+            <motion.h2
+               whileHover={{ scale: 1.2 }}
+               transition={{ type: "spring", stiffness: 500 }}
+               className="close"
+               onClick={() => setStatus("none")}
+            >
+               X
+            </motion.h2>
          </Container>
       </AnimatePresence>
    );
 }
 
 const Container = styled(motion.div)`
-   width: 75%;
-   height: 85%;
+   width: 80%;
+   height: 87%;
    border-radius: 19px;
    border: 1px solid ${(props) => props.border};
    background: linear-gradient(
@@ -44,6 +53,19 @@ const Container = styled(motion.div)`
          ${(props) => props.modal2_13} 102.76%
       );
    z-index: 2;
+   position: relative;
+
+   .close {
+      position: absolute;
+      z-index: 2;
+      top: 1.5%;
+      right: 3%;
+      user-select: none;
+      -moz-user-select: none;
+      -khtml-user-select: none;
+      -webkit-user-select: none;
+      -o-user-select: none;
+   }
 `;
 
 const Bottom = styled.div``;
